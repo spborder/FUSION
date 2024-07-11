Finally fixing annotations scaling for different tile sizes

FUSION_Handlers.py
@@ -151,7 +151,7 @@ class LayoutHandler:
             tile_size = 240
             slide_properties = []
             combined_colors_dict = {}
-            zoom_levels = 8
+            zoom_levels = 3
             map_bounds = [[0,240],[0,240]]
         
         center_point = [0.5*(map_bounds[0][0]+map_bounds[1][0]),0.5*(map_bounds[0][1]+map_bounds[1][1])]
@@ -161,10 +161,10 @@ class LayoutHandler:
                 id = 'slide-tile-holder',
                 children = [
                     dl.TileLayer(
-                        id = 'slide-tile',
+                        id = f'slide-tile{np.random.randint(0,100)}',
                         url = map_url,
                         tileSize = tile_size,
-                        maxNativeZoom=zoom_levels-1
+                        maxNativeZoom=zoom_levels-2
                     )
                 ]
             ),
@@ -196,7 +196,7 @@ class LayoutHandler:
         ]
 
         map_layer = dl.Map(
-            center = center_point, zoom = 3, minZoom = 0, crs='Simple',bounds = map_bounds,
+            center = center_point, zoom = 0, minZoom = 0, crs='Simple',
             style = {'width':'100%','height':'90vh','margin':'auto','display':'inline-block'},
             id = 'slide-map',
             preferCanvas=True,

FUSION_Utils.py
@@ -51,16 +51,25 @@ def extract_overlay_value(structure_list,overlay_prop):
                     if overlay_prop['value'] in st[overlay_prop['name']]:
                         if not overlay_prop['sub_value'] is None:
                             if overlay_prop['sub_value'] in st[overlay_prop['name']][overlay_prop['value']]:
-                                raw_values_list.append(float(st[overlay_prop['name']][overlay_prop['value']][overlay_prop['sub_value']]))
+                                if not type(st[overlay_prop['name']][overlay_prop['sub_value']])==str:
+                                    raw_values_list.append(float(st[overlay_prop['name']][overlay_prop['value']][overlay_prop['sub_value']]))
+                                else:
+                                    raw_values_list.append(st[overlay_prop['name']][overlay_prop['value']][overlay_prop['sub_value']])
                         else:
-                            raw_values_list.append(float(st[overlay_prop['name']][overlay_prop['value']]))
+                            if not type(st[overlay_prop['name']][overlay_prop['value']])==str:
+                                raw_values_list.append(float(st[overlay_prop['name']][overlay_prop['value']]))
+                            else:
+                                raw_values_list.append(st[overlay_prop['name']][overlay_prop['value']])
                     elif overlay_prop['value']=='max':
                         # Getting the max index of st[overlay_prop['name']][overlay_prop['value']]
                         check_values = list(st[overlay_prop['name']].values())
                         raw_values_list.append(np.argmax(check_values))
                 else:
-                    raw_values_list.append(float(st[overlay_prop['name']]))
-    
+                    if not type(st[overlay_prop['name']])==str:
+                        raw_values_list.append(float(st[overlay_prop['name']]))
+                    else:
+                        raw_values_list.append(st[overlay_prop['name']])
+
     return raw_values_list
 
 def gen_violin_plot(feature_data, label_col, label_name, feature_col, custom_col):